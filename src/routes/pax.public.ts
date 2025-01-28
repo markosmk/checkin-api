89064c5fb549193da1a82037c670cf4ed510e94a
Markos Mk <markosmk@me.com>
2025-01-28 16:46:15 -0300

    feat: implement public pax routes for create, update, and delete operations

---
+++ src/routes/pax.public.ts
@@ -0,0 +1,93 @@
+import { Hono } from "hono"
+import { HTTPException } from "hono/http-exception"
+import { zValidator } from "@hono/zod-validator"
+
+import prismaClients from "../lib/prisma"
+import { Variables } from "../types/jwt"
+import { createPaxSchema } from "./pax"
+import { verifyToken } from "../utils/security"
+
+const publicPax = new Hono<{ Bindings: Env; Variables: Variables }>()
+
+// create empty pax
+publicPax.post("/:bookingId", async (c) => {
+  const bookingId = c.req.param("bookingId")
+  if (!bookingId) throw new HTTPException(400, { message: "Booking es necesario." })
+
+  const token = c.req.header("X-Reservation-Token")
+  if (!token) throw new HTTPException(403, { message: "Acceso denegado." })
+
+  const decoded = await verifyToken(token, c.env.SECRET_KEY_RESERVATION)
+  if (!decoded) throw new HTTPException(403, { message: "Token inválido o expirado." })
+
+  const prisma = await prismaClients.fetch(c.env.DB)
+  const booking = await prisma.booking.findFirst({
+    where: { id: bookingId, reservationId: decoded.reservationId, hotelId: decoded.hotelId },
+  })
+  if (!booking) throw new HTTPException(404, { message: "Pax no encontrado." })
+
+  const newPax = await prisma.pax.create({
+    data: {
+      bookingId: booking.id,
+      firstname: "",
+      lastname: "",
+      email: "",
+      phone: "",
+      birthdate: "",
+      docType: "",
+      docNumber: "",
+    },
+  })
+  return c.json(newPax)
+})
+
+const updatePaxSchema = createPaxSchema.partial()
+publicPax.put("/:id", zValidator("json", updatePaxSchema), async (c) => {
+  const data = c.req.valid("json")
+
+  const paxId = c.req.param("id")
+  if (!paxId) throw new HTTPException(400, { message: "Pax es necesario." })
+
+  const token = c.req.header("X-Reservation-Token")
+  if (!token) throw new HTTPException(403, { message: "Acceso denegado." })
+
+  const decoded = await verifyToken(token, c.env.SECRET_KEY_RESERVATION)
+  if (!decoded) throw new HTTPException(403, { message: "Token inválido o expirado." })
+
+  const prisma = await prismaClients.fetch(c.env.DB)
+  const pax = await prisma.pax.findFirst({
+    where: { id: paxId, booking: { reservationId: decoded.reservationId, hotelId: decoded.hotelId } },
+  })
+  if (!pax) throw new HTTPException(404, { message: "Pax no encontrado." })
+
+  const updatedPax = await prisma.pax.update({
+    where: { id: paxId },
+    data: {
+      ...data,
+      bookingId: pax.bookingId,
+    },
+  })
+  return c.json(updatedPax)
+})
+
+publicPax.delete("/:id", async (c) => {
+  const paxId = c.req.param("id")
+  if (!paxId) throw new HTTPException(400, { message: "El paxId es necesario." })
+
+  const token = c.req.header("X-Reservation-Token")
+  if (!token) throw new HTTPException(403, { message: "Acceso denegado." })
+
+  const decoded = await verifyToken(token, c.env.SECRET_KEY_RESERVATION)
+  if (!decoded) throw new HTTPException(403, { message: "Token inválido o expirado." })
+
+  const prisma = await prismaClients.fetch(c.env.DB)
+  const pax = await prisma.pax.findFirst({
+    where: { id: paxId, booking: { reservationId: decoded.reservationId, hotelId: decoded.hotelId } },
+  })
+  if (!pax) throw new HTTPException(404, { message: "Pax no encontrado." })
+
+  await prisma.pax.delete({ where: { id: paxId } })
+  return c.json({ message: "Pax eliminado correctamente." })
+})
+
+export default publicPax
