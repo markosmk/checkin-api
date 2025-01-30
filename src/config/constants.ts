d357188cfe5b047319fe2c8e13334c8e584bce1c
Markos Mk <markosmk@me.com>
2025-01-30 19:07:55 -0300

    feat: add constants for time-related settings

---
+++ src/config/constants.ts
@@ -0,0 +1,8 @@
+/** 24 hours */
+export const EXPIRE_TIME_VERIFICATION = 1000 * 60 * 60 * 24
+/** 1 hour */
+export const EXPIRE_TIME_RESET_PASSWORD = 1000 * 60 * 60 * 1
+/** 5 min */
+export const TIME_THROTTLE = 1000 * 60 * 5
+/** 30 days */
+export const TRIAL_PERIOD = 1000 * 60 * 60 * 24 * 30
