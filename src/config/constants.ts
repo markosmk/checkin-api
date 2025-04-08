ca6e51dae8aa1ed2aca576256762992299a25c94
Markos Mk <markosmk@me.com>
2025-04-08 14:59:37 -0300

    feat: update dependencies and configuration for improved project structure

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,8 +1,13 @@
-/** 24 hours */
-export const EXPIRE_TIME_VERIFICATION = 1000 * 60 * 60 * 24
-/** 1 hour */
-export const EXPIRE_TIME_RESET_PASSWORD = 1000 * 60 * 60 * 1
+/** 10 hours */
+export const EXPIRE_TIME_VERIFICATION = 1000 * 60 * 60 * 10 // or only 15min
+/** 2 hours */
+export const EXPIRE_TIME_RESET_PASSWORD = 1000 * 60 * 60 * 2
 /** 5 min */
 export const TIME_THROTTLE = 1000 * 60 * 5
 /** 30 days */
 export const TRIAL_PERIOD = 1000 * 60 * 60 * 24 * 30
+/** 30 days in milliseconds **/
+export const TIME_EXPIRE_SESSION = 1000 * 60 * 60 * 24 * 30
+export const NAME_COOKIE_SESSION = "auth_app_session"
+/** use veirifcation mail, if is false, user not need verify... maybe move to env */
+export const VERIFY_EMAIL = true
