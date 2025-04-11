12f3acba078bf6a9aac6b460d19c7cf77b6d74ca
Markos Mk <markosmk@me.com>
2025-04-11 15:10:32 -0300

    feat: simplify cuid schema creation with reusable cuidSchema

--- src/utils/create-cuid-schema.ts
+++ src/utils/create-cuid-schema.ts
@@ -1,18 +1,13 @@
 import z from "zod"
 import { isCuid } from "@paralleldrive/cuid2"
 
+export const cuidSchema = z
+  .string()
+  .min(24, "The CUID must be at least 24 characters long.")
+  .max(24, "The CUID must be exactly 24 characters long.")
+  .refine((val) => isCuid(val), {
+    message: "Invalid CUID format",
+  })
+
 export const createCuidSchema = (params: string | string[]) =>
-  z.object(
-    Object.fromEntries(
-      (Array.isArray(params) ? params : [params]).map((param) => [
-        param,
-        z
-          .string()
-          .min(24, "The CUID must be at least 24 characters long.")
-          .max(24, "The CUID must be exactly 24 characters long.")
-          .refine((val) => isCuid(val), {
-            message: "Invalid CUID format",
-          }),
-      ])
-    )
-  )
+  z.object(Object.fromEntries((Array.isArray(params) ? params : [params]).map((param) => [param, cuidSchema])))
