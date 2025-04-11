d5adc13dc7aacbc764ad3e23406b29b1d570620c
Markos Mk <markosmk@me.com>
2025-04-11 16:27:36 -0300

    fix: update userId retrieval to use session instead of jwtPayload

--- src/modules/hotels/hotels.routes.ts
+++ src/modules/hotels/hotels.routes.ts
@@ -8,43 +8,43 @@ import * as hotelService from "./hotels.service"
 const hotel = new Hono<App>()
 
 hotel.get("/", async (c) => {
-  const userId = c.get("jwtPayload")?.userId
+  const userId = c.get("session")?.userId
   const hotels = await hotelService.getHotels(c.get("db"), userId)
   return c.json(hotels)
 })
 
 hotel.get("/:id", zValidator("param", createCuidSchema("id")), async (c) => {
-  const userId = c.get("jwtPayload").userId
-  const hotelId = c.req.param("id")
+  const userId = c.get("session").userId
+  const hotelId = c.req.valid("param").id
   const hotel = await hotelService.getHotel(c.get("db"), userId, hotelId)
   return c.json(hotel)
 })
 
 hotel.get("/:id/bookings", zValidator("param", createCuidSchema("id")), async (c) => {
-  const userId = c.get("jwtPayload")?.userId
-  const hotelId = c.req.param("id")
+  const userId = c.get("session")?.userId
+  const hotelId = c.req.valid("param").id
   const bookings = await hotelService.getBookingsByHotelId(c.get("db"), userId, hotelId)
   return c.json(bookings)
 })
 
 hotel.post("/", zValidator("json", createHotelSchema), async (c) => {
-  const userId = c.get("jwtPayload")?.userId
+  const userId = c.get("session")?.userId
   const data = c.req.valid("json")
   const hotel = await hotelService.createHotel(c.get("db"), userId, data)
   return c.json({ message: "Hotel creado con éxito", hotel }, 201)
 })
 
 hotel.put("/:id", zValidator("param", createCuidSchema("id")), zValidator("json", updateHotelSchema), async (c) => {
-  const userId = c.get("jwtPayload")?.userId
+  const userId = c.get("session")?.userId
   const data = c.req.valid("json")
-  const hotelId = c.req.param("id")
+  const hotelId = c.req.valid("param").id
   const hotel = await hotelService.updateHotel(c.get("db"), userId, hotelId, data)
   return c.json({ message: "Hotel actualizado con éxito", hotel })
 })
 
 hotel.delete("/:id", zValidator("param", createCuidSchema("id")), async (c) => {
-  const userId = c.get("jwtPayload")?.userId
-  const hotelId = c.req.param("id")
+  const userId = c.get("session")?.userId
+  const hotelId = c.req.valid("param").id
   await hotelService.deleteHotel(c.get("db"), userId, hotelId)
   return c.json({ message: "Hotel eliminado con éxito" })
 })
