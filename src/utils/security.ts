514b2f82450a137f70f19d3b3299a50c220c542e
Markos Mk <markosmk@me.com>
2025-04-11 15:23:51 -0300

    feat: simplify CustomJWTPayload interface and update createToken function

--- src/utils/security.ts
+++ src/utils/security.ts
@@ -1,23 +1,17 @@
 import { sign, verify } from "hono/jwt"
 import { JWTPayload } from "hono/utils/jwt/types"
+import { TokenPayload } from "../types"
 
-interface CustomJWTPayload extends JWTPayload {
-  bookingId: string
-  reservationId: string
-  hotelId: string
-}
+export interface CustomJWTPayload extends JWTPayload, TokenPayload {}
 
-export async function createToken(
-  data: Record<string, string>,
-  secret: string,
-  expInMinutes: number = 5
-): Promise<string> {
+export async function createToken(data: TokenPayload, secret: string, expInMinutes: number = 5): Promise<string> {
   const payload = {
     ...data,
     exp: Math.floor(Date.now() / 1000) + 60 * expInMinutes,
   }
   return await sign(payload, secret)
 }
+
 export async function verifyToken(token: string, secret: string): Promise<CustomJWTPayload | null> {
   try {
     return (await verify(token, secret)) as CustomJWTPayload
