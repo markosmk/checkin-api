cfd196aab5deba1c42374a0c6ef38d20d261eeea
Markos Mk <markosmk@me.com>
2025-04-11 15:09:33 -0300

    feat: add types and enums for client data and booking status

---
+++ src/db/enum.ts
@@ -0,0 +1,55 @@
+export type ClientData = {
+  firstname: string
+  lastname: string
+  email: string
+}
+
+export type AdditionalHotelProps = {
+  rooms?: number
+  stars?: number
+  amenities?: string[]
+  // rooms: integer("rooms"), // unnecesary
+  // website: text("website"),
+  // location: text("location"), // SQLite doesn't support jsonb, storing as text
+}
+
+export const BookingStatus = {
+  PENDING: "pending",
+  CONFIRMED: "confirmed",
+  CANCELLED: "cancelled",
+  COMPLETED: "completed",
+  UNKNOWN: "unknown",
+} as const
+export type BookingStatus = (typeof BookingStatus)[keyof typeof BookingStatus]
+
+export enum FieldsAvailable {
+  CAR_MODEL = "carModel",
+  CAR_PLATE = "carPlate",
+  COUNTRY = "country",
+  CITY = "city",
+  ZIP_CODE = "zipCode",
+  ADDRESS = "address",
+  GENDER = "gender",
+}
+
+// this may change without changes DB
+export type AdditionalPaxProps = {
+  departureDate?: string | Date // primary
+  carModel?: string // primary
+  carPlate?: string // primary
+  country?: string
+  city?: string
+  zipCode?: string
+  address?: string
+  gender?: string
+}
+
+export type FilesBooking = {
+  docFrontPath: string
+  docBackPath: string
+  passportPath: string
+  pdfPath: string
+}
+
+// to customize colors, or style page..
+export type OptionsHotel = {}
