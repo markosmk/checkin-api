470ae858f56476d5c37732f3d4ad296af2c69195
Markos Mk <markosmk@me.com>
2025-04-11 15:25:33 -0300

    feat: add TokenPayload interface for public routes

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,28 @@
 import { JWTPayload } from "hono/utils/jwt/types"
 import type * as schema from "./db/schema"
 import type { DrizzleD1Database } from "drizzle-orm/d1"
-
+import { BookingStatus } from "./db/enum"
 // import type * as schemaPG from "./db/schema.pg"
 // import type { NodePgDatabase } from "drizzle-orm/node-postgres"
 
 export type DB = DrizzleD1Database<typeof schema>
 // export type DBPG = NodePgDatabase<typeof schemaPG>
+
+/** used in public routes, to send in bearer token */
+export interface TokenPayload {
+  booking: {
+    id: string
+    // reservationId: string
+    checkin: string
+    status: BookingStatus
+    maxPaxs: number | null
+  }
+  hotel: {
+    id?: string
+    name: string
+    slug: string
+  }
+}
 
 export type App = {
   Bindings: Env // Env is generate with comand pn wrangler types (include bindings d1, with name DB_D1)
@@ -16,9 +32,12 @@ export type App = {
     // dbTEST: DBTEST // -> Testing
     user: schema.User
     session: schema.Session
-  }
+    // booking: { id: string; checkin: string; checkout?: string; status: BookingStatus; maxPaxs: number | null }
+    // hotel: { id?: string; slug: string; name: string }
+  } & TokenPayload
 }
 
+/** used in internal site panel, as additional bearer token if cookies is not working.. */
 export type CustomPayload = {
   sub: string
   sessionToken: string
