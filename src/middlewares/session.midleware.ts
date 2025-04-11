9177a6724b89798e13a017c00aa93c09a99c71f0
Markos Mk <markosmk@me.com>
2025-04-11 15:25:21 -0300

    feat: implement session middleware with origin verification and cookie handling

--- src/middlewares/session.midleware.ts
+++ src/middlewares/session.midleware.ts
@@ -3,8 +3,30 @@ import { getCookie } from "hono/cookie"
 import { NAME_COOKIE_SESSION } from "../config/constants"
 import * as sessionService from "../modules/auth/session.service"
 import * as cookieService from "../modules/auth/cookie.service"
+import { env } from "hono/adapter"
+import { verifyRequestOrigin } from "../utils/helper"
+import { App } from "../types"
 
-export const sessionMiddleware = async (c: Context, next: Next) => {
+export const sessionMiddleware = async (c: Context<App>, next: Next) => {
+  // exclude auth routes
+  if (c.req.path.includes("/auth")) {
+    return next()
+  }
+
+  const originHeader = c.req.header("Origin") ?? c.req.header("origin")
+  const hostHeader = c.req.header("Host") ?? c.req.header("X-Forwarded-Host")
+  // this is because of cloudflare workers, we need to allow cross origin requests for public routes
+  if (
+    (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [hostHeader, env(c).FRONTEND_URL])) &&
+    env(c).ENVIRONMENT === "production" &&
+    c.req.method !== "GET"
+  ) {
+    return new Response(null, {
+      status: 403,
+    })
+  }
+
+  // 1) get session id (token)
   let token = getCookie(c, NAME_COOKIE_SESSION) ?? null
 
   if (!token) {
@@ -15,23 +37,40 @@ export const sessionMiddleware = async (c: Context, next: Next) => {
   if (!token) {
     return c.json({ message: "Unauthorized" }, 401)
   }
+
+  // 2) validate session
   const { session, user } = await sessionService.validateSessionToken(c.get("db"), token)
 
   if (!session || !user || session === null) {
-    c.header("Set-Cookie", cookieService.createBlankSessionCookie(), {
-      append: true,
-    })
+    c.header(
+      "Set-Cookie",
+      cookieService.createBlankSessionCookie({
+        secure: c.env.ENVIRONMENT !== "development",
+      }),
+      {
+        append: true,
+      }
+    )
     return c.json({ message: "Unauthorized" }, 401)
   }
 
-  // keep the session alive, extends the expiration date
-  if (session.fresh) {
-    c.header("Set-Cookie", cookieService.createSessionCookie(token), {
-      append: true,
-    })
+  // 3) keep the session alive, extends the expiration date
+  if (session?.fresh) {
+    c.header(
+      "Set-Cookie",
+      cookieService.createSessionCookie(token, {
+        secure: env(c).ENVIRONMENT !== "development",
+      }),
+      {
+        append: true,
+      }
+    )
   }
 
+  // 4) set the user and session in the context
   c.set("user", user)
   c.set("session", session)
+
+  // await next();
   return next()
 }
