88460c16228833332991c699f70eea4ded348c68
Markos Mk <markosmk@me.com>
2025-04-15 10:57:19 -0300

    chore: update import to use type for ContentfulStatusCode

--- src/utils/error-handle.ts
+++ src/utils/error-handle.ts
@@ -1,7 +1,7 @@
 import type { Context } from "hono"
 import { HTTPException } from "hono/http-exception"
 import type { HTTPResponseError } from "hono/types"
-import { ContentfulStatusCode } from "hono/utils/http-status"
+import type { ContentfulStatusCode } from "hono/utils/http-status"
 import { ZodError } from "zod"
 
 export class AppException extends HTTPException {
