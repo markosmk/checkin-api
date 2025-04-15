b5bce66a26eaa1ee27c17263f5ee7f66c28ea2dd
Markos Mk <markosmk@me.com>
2025-04-15 10:57:14 -0300

    fix: simplify regex for deleting accents in slugify function

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -3,7 +3,10 @@ export function slugify(text: string): string {
     .toString()
     .toLowerCase()
     .normalize("NFD") // separate accents from letters
-    .replace(/[\u0300-\u036f]/g, "") // delete all accents
+    .replace(
+      /\u0300|\u0301|\u0302|\u0303|\u0304|\u0305|\u0306|\u0307|\u0308|\u0309|\u030a|\u030b|\u030c|\u030d|\u030e|\u030f|\u0310|\u0311|\u0312|\u0313|\u0314|\u0315|\u0316|\u0317|\u0318|\u0319|\u031a|\u031b|\u031c|\u031d|\u031e|\u031f|\u0320|\u0321|\u0322|\u0323|\u0324|\u0325|\u0326|\u0327|\u0328|\u0329|\u032a|\u032b|\u032c|\u032d|\u032e|\u032f|\u0330|\u0331|\u0332|\u0333|\u0334|\u0335|\u0336|\u0337|\u0338|\u0339|\u033a|\u033b|\u033c|\u033d|\u033e|\u033f|\u0340|\u0341|\u0342|\u0343|\u0344|\u0345|\u0346|\u0347|\u0348|\u0349|\u034a|\u034b|\u034c|\u034d|\u034e|\u034f|\u0350|\u0351|\u0352|\u0353|\u0354|\u0355|\u0356|\u0357|\u0358|\u0359|\u035a|\u035b|\u035c|\u035d|\u035e|\u035f|\u0360|\u0361|\u0362|\u0363|\u0364|\u0365|\u0366|\u0367|\u0368|\u0369|\u036a|\u036b|\u036c|\u036d|\u036e|\u036f/g,
+      ""
+    ) // delete all accents
     .replace(/\s+/g, "-")
     .replace(/[^\w-]+/g, "")
     .replace(/--+/g, "-")
@@ -11,7 +14,7 @@ export function slugify(text: string): string {
     .replace(/-+$/, "")
 }
 
-export function generateCode(email: string, maxLength: number = 20): string {
+export function generateCode(email: string, maxLength = 20): string {
   const username = email.split("@")[0]
   const maxUsernameLength = maxLength - 5
   const trimmedUsername = username.slice(0, maxUsernameLength)
@@ -50,10 +53,10 @@ export function getDeviceInfo(userAgent: string): string {
   if (!userAgent) return "Desconocido - Desconocido"
 
   const ua = userAgent.toLowerCase()
-  let os = "Desconocido",
-    osVersion = "Desconocido"
-  let device = "PC",
-    manufacturer = "Desconocido"
+  let os = "Desconocido"
+  let osVersion = "Desconocido"
+  let device = "PC"
+  let manufacturer = "Desconocido"
   let browser = "Desconocido"
 
   // system
